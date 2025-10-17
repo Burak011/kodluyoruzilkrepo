e47c18756d1eebb9111b5dfa2fbfe8626b832642
Doğukan Burak Çelikten <dcelikten01@icloud.com>
2025-10-17 15:03:30 +0300

    java-1

---
+++ degisken_tanimlama.js
@@ -0,0 +1,43 @@
+let password = "1234";
+console.log(password);
+
+
+let fullName = "John Doe";
+console.log(fullName);
+
+let price=30;
+let tax=0.18;
+let totalTax=price*tax;
+console.log("Fiyat = ",price," KDV = ",tax," Toplam KDV = ",totalTax);
+
+let sayi1=2;
+console.log(sayi1**2); //sayi1'in karesini alır
+
+console.log(Math.floor(1.6)); //Aşağı yuvarlama
+console.log(Math.ceil(1.2));  //Yukarı yuvarlama
+console.log(Math.round(1.5)); //En yakın sayıya yuvarlama
+
+
+let isActive = false;
+console.log(isActive);
+
+let a;
+Boolean(a); //undefined
+
+const B=0;
+console.log(Boolean(B));
+
+var y="" || -2 || 'JavaScript';
+console.log(y);
+
+var z ={2:'js'};
+console.log(Boolean(z));
+
+var x =10/ 's';
+console.log(Boolean(x));
+
+var t ="" && -2 &&  'JavaScript';
+console.log(Boolean(t));
+
+var w= false || 0;
+console.log(Boolean(w));
